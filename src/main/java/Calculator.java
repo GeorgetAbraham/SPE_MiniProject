e5985cbc1127b2b8209a471ec72b131042c14eea
imt2017019 <georget.abraham@iiitb.org>
2021-03-07 01:55:20 +0530

    temporary calculator with add and div with tests

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator{
+    public float add(float a,float b){
+        return a+b;
+    }
+    public float divide(float a,float b){
+        return a/b;
+    }
+
+
+
+    public static void main(String[] args){
+        System.out.println("Hello friend");
+    }
+}
